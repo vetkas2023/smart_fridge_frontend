745407d6b7e28a3b99d66329f866739a80d8a667
Vetka Serdyuk <serdyuk2007mail.ru@yandex.ru>
2025-01-30 22:23:27 +0300

    fix config again

--- app/config.js
+++ app/config.js
@@ -2,11 +2,8 @@
 // Файл констант приложения
 // Удобно, особенно когда не помнишь везде ли поменял значение переменной 👍
 // 
-
-import Config from "react-native-config"
-
-const host = Config.API_HOST || 'http://localhost'
-const port = Config.API_PORT || 8000
+const host = process.env.EXPO_PUBLIC_API_HOST || 'http://localhost'
+const port = process.env.EXPO_PUBLIC_API_PORT || 8000
 
 export const URL = `${host}:${port}/api/v1`;
 
